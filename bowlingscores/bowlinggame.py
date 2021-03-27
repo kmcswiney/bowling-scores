a72899d7d5bca59e048c13760e07fe30d291008d
Kieran McSwiney <kieran.mcswiney@techmedia.com.au>
2021-03-27 14:10:01 +1100

    Make frame field private, rename frame_num to frame_index for clarity

--- bowlingscores/bowlinggame.py
+++ bowlingscores/bowlinggame.py
@@ -23,48 +23,48 @@ class Frame:
 class BowlingGame:
 
     def __init__(self):
-        self.frames = []
+        self._frames = []
 
     def roll(self, pins):
         if self._is_first_roll() or self._previous_frame().is_complete():
-            self.frames.append(Frame(rolls=[pins]))
+            self._frames.append(Frame(rolls=[pins]))
         else:
             self._previous_frame().rolls.append(pins)
 
     def score(self):
-        return sum(self._compute_frame_score(frame_num, frame) for frame_num, frame in enumerate(self.frames))
+        return sum(self._compute_frame_score(frame_index, frame) for frame_index, frame in enumerate(self._frames))
 
-    def _compute_frame_score(self, frame_num, frame):
-        return self._compute_completed_frame_score(frame_num, frame) if frame.is_complete() else 0
+    def _compute_frame_score(self, frame_index, frame):
+        return self._compute_completed_frame_score(frame_index, frame) if frame.is_complete() else 0
 
-    def _compute_completed_frame_score(self, frame_num, frame):
+    def _compute_completed_frame_score(self, frame_index, frame):
         if frame.is_spare():
-            return self._compute_spare_score(frame_num, frame)
+            return self._compute_spare_score(frame_index, frame)
         elif frame.is_strike():
-            return self._compute_strike_score(frame_num, frame)
+            return self._compute_strike_score(frame_index, frame)
         else:
             return frame.total_pins()
 
-    def _compute_spare_score(self, frame_num, frame):
-        following_roll = self._next_rolls(frame_num, 1)
+    def _compute_spare_score(self, frame_index, frame):
+        following_roll = self._next_rolls(frame_index, 1)
         if following_roll:
             return frame.total_pins() + sum(following_roll)
         else:
             return 0
 
-    def _compute_strike_score(self, frame_num, frame):
-        following_2_rolls = self._next_rolls(frame_num, 2)
+    def _compute_strike_score(self, frame_index, frame):
+        following_2_rolls = self._next_rolls(frame_index, 2)
         if following_2_rolls:
             return frame.total_pins() + sum(following_2_rolls)
         else:
             return 0
 
-    def _next_rolls(self, frame_num, num_rolls):
+    def _next_rolls(self, frame_index, num_rolls):
         """ 
         Given a frame index number, return the following num_rolls rolls that occurred after the given frame.
         If the specified number of rolls have not occurred yet, return None.
         """
-        following_frames = self.frames[frame_num + 1:]
+        following_frames = self._frames[frame_index + 1:]
         rolls = [roll for frame in following_frames for roll in frame.rolls]
         if len(rolls) >= num_rolls:
             return rolls[:num_rolls]
@@ -72,7 +72,7 @@ class BowlingGame:
             return None
 
     def _previous_frame(self):
-        return self.frames[-1]
+        return self._frames[-1]
 
     def _is_first_roll(self):
-        return not self.frames
+        return not self._frames
