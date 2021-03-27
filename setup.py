c0219a8e6d71e5d28534537760934f9823ddb283
Kieran McSwiney <kieran.mcswiney@techmedia.com.au>
2021-03-27 11:25:04 +1100

    Setup project structure

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name="bowlingscores",
+    version="0.1.0",
+    author="Kieran McSwiney",
+    packages=["bowlingscores"]
+)
---
+++ tests/test_bowlinggame.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+class TestBowlingGame(TestCase):
+
+    def test_nothing(self):
+        pass
