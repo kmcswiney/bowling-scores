f2a44639412b762d886987bc0dc7b98e1b7ec696
Kieran McSwiney <kieran.mcswiney@techmedia.com.au>
2021-03-27 12:20:55 +1100

    Refactor for readability

--- bowlingscores/bowlinggame.py
+++ bowlingscores/bowlinggame.py
@@ -18,10 +18,16 @@ class BowlingGame:
         self.frames = []
 
     def roll(self, pins):
-        if not self.frames or self.frames[-1].is_complete():
+        if self._is_first_roll() or self._previous_frame().is_complete():
             self.frames.append(Frame(rolls=[pins]))
         else:
-            self.frames[-1].rolls.append(pins)
+            self._previous_frame().rolls.append(pins)
 
     def score(self):
         return sum(frame.score() for frame in self.frames if frame.is_complete())
+
+    def _previous_frame(self):
+        return self.frames[-1]
+
+    def _is_first_roll(self):
+        return not self.frames
